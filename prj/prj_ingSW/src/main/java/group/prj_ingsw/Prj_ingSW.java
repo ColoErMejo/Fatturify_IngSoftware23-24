f998f7bd2197195e873258f1774a143cde579331
Merluz <146435667+Merluz@users.noreply.github.com>
2023-11-29 11:57:45 +0100

    Add files via upload
    importare in eclipse come maven prj

---
+++ prj/prj_ingSW/src/main/java/group/prj_ingsw/Prj_ingSW.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group.prj_ingsw;
+
+/**
+ *
+ * @author merlo
+ */
+public class Prj_ingSW {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
